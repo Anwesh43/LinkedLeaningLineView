14ad87a9b6c4ced1a2311e83390474d8f9c2a5ca
Anwesh43 <anweshthecool0@gmail.com>
2019-12-25 21:13:27 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedleaninglineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedleaninglineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedleaninglineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.leaninglineview.LeaningLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LeaningLineView.create(this)
     }
 }
