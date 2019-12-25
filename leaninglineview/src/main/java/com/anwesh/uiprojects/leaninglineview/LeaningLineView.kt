567d9d98f026ba16bceadcb51f8a8ae79b729b89
Anwesh43 <anweshthecool0@gmail.com>
2019-12-25 21:03:20 +0530

    adding LLNode class

--- leaninglineview/src/main/java/com/anwesh/uiprojects/leaninglineview/LeaningLineView.kt
+++ leaninglineview/src/main/java/com/anwesh/uiprojects/leaninglineview/LeaningLineView.kt
@@ -128,4 +128,46 @@ class LeaningLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LLNode(var i : Int, val state : State = State()) {
+
+        private var next : LLNode? = null
+        private var prev : LLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LLNode {
+            var curr : LLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
