5a06348e4987c12941e1c7f3a89758f11879616d
Anwesh43 <anweshthecool0@gmail.com>
2019-12-25 20:54:47 +0530

    adding Canvas extension methods

--- leaninglineview/src/main/java/com/anwesh/uiprojects/leaninglineview/LeaningLineView.kt
+++ leaninglineview/src/main/java/com/anwesh/uiprojects/leaninglineview/LeaningLineView.kt
@@ -21,9 +21,44 @@ val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 30
+val sizeFactor : Float = 2.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (Math.PI / 2) * this).toFloat()
+
+fun Canvas.drawLeaningLine(i : Int, scale : Float, gap : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, lines)
+    save()
+    translate(i * gap, 0f)
+    rotate(rotation * sf)
+    drawLine(0f, 0f, 0f, -size, paint)
+    restore()
+}
+
+fun Canvas.drawLeaningLines(scale : Float, w : Float, size : Float, paint : Paint) {
+    val sc : Float = scale.divideScale(1, 2).cosify()
+    val offsetW : Float = w * offset
+    val start : Float = (w - offsetW) / 2
+    val gap : Float = offsetW / lines
+    save()
+    translate(start, 0f)
+    for (j in 0..(lines - 1)) {
+        drawLeaningLine(j, scale, gap, size, paint)
+    }
+    drawLine(offsetW * (1 - sc), 0f, offsetW, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawLLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size = gap / sizeFactor
+    save()
+    translate(0f, gap * (i + 1))
+    drawLeaningLines(scale, w, size, paint)
+    restore()
+}
