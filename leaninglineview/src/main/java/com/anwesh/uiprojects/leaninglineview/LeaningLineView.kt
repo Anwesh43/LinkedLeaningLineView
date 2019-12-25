8a2fa9d5409fd7baebb864967c24f6560b2f5f64
Anwesh43 <anweshthecool0@gmail.com>
2019-12-25 20:44:43 +0530

    adding constants and libraries

--- leaninglineview/src/main/java/com/anwesh/uiprojects/leaninglineview/LeaningLineView.kt
+++ leaninglineview/src/main/java/com/anwesh/uiprojects/leaninglineview/LeaningLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.leaninglineview
 /**
  * Created by anweshmishra on 25/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val rotation : Float = 45f
+val offset : Float = 0.8f
+val scGap : Float = 0.5f
+val lines : Int = 6
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
