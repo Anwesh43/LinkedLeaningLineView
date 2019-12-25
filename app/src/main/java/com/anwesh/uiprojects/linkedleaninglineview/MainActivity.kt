218fc1df91bd3b0d79303c86e264ce7f236b6ec2
Anwesh43 <anweshthecool0@gmail.com>
2019-12-25 21:14:12 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedleaninglineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedleaninglineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedleaninglineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.leaninglineview.LeaningLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LeaningLineView.create(this)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
